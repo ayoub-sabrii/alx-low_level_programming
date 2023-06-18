f3fe8b52cba58e421150b4048e4cee53548c5ed2
ayoub-sabrii <ayoob.sabrii@gmail.com>
2023-06-18 11:55:41 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - programme print all numbers
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	while (a <= 7)
+	{
+		b = a + 1;
+		while (b <= 8)
+		{
+			c = b + 1;
+			while (c <= 9)
+			{
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (c != 7)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
