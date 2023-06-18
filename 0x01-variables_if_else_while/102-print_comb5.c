fc31389cd329789880bb640f11425499b01e29b4
ayoub-sabrii <ayoob.sabrii@gmail.com>
2023-06-18 14:39:25 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - adv
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b;
+
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			putchar(a / 10 + 48);
+			putchar(a % 10 + 48);
+			putchar(' ');
+			putchar(b % 10 + 48);
+			if (a != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			b++;
+		}
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
