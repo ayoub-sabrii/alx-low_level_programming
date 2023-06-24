8c51e19d5ea1d5190c363455d9eeb4013ca76979
ayoub-sabrii <ayoob.sabrii@gmail.com>
2023-06-23 17:26:29 -0700

    print nmbrs

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * print_nmbr - print 0 to 9
- *
- * using _putchar
- *
- * Return: Always 0
+ * print_numbers - print 0-9
+ * return: void
  */
 
 void print_numbers(void)
 {
-	char c;
-
-	for (c = '0'; c <= '9'; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+char c;
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
 }
