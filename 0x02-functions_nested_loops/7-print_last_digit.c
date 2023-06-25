fcf2b6bd5c54ae215c3d4d9542dcdeb2d4a15e1b
ayoub-sabrii <ayoob.sabrii@gmail.com>
2023-06-25 07:12:35 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,13 @@
 
 int print_last_digit(int n)
 {
-	int lastdigit = n % 10;
+	int lastdigit;
 
-	_putchar(lastdigit);
+	if (n < 0)
+		lastdigit = -1 * (n % 10);
+	else
+		lastdigit = n % 10;
+
+	_putchar(lastdigit + '0');
 	return (lastdigit);
 }
